77dab9d3d2b40244e1863faac02d359ec07d5d23
AdamKahoe <89557570+AdamKahoe@users.noreply.github.com>
2021-12-03 14:26:10 -0800

    Add files via upload

---
+++ Functions Intermediate 1.py
@@ -0,0 +1,28 @@
+x = [ [5,2,3], [10,8,9] ]
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 20, 'y': 30} ]
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+print("iterateDictionary",)
+'iterateDictionary'(students)
+
+dojo = {
+'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def(printInfo)
+    print(printInfo)
+
+printInfo(dojo)
